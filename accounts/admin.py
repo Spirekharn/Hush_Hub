1344ef0bf9b637f24616e703a43a6232e3f491fa
Spirekharn <142277264+Spirekharn@users.noreply.github.com>
2025-11-19 08:02:23 +0600

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import DailyAffirmation, DailyAffirmationLog, EmergencyContact
-
-admin.site.register(DailyAffirmation)
-admin.site.register(DailyAffirmationLog)
-admin.site.register(EmergencyContact)
